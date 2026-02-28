8f407b1a10994e880c4940f386a4bcec4f0ea587
Kira <tobiorin1@gmail@gmail.com>
2026-02-28 18:26:27 +0300

    Создала школу

---
+++ school_1.py
@@ -0,0 +1,6 @@
+class Teacher:
+    def __init__(self, name, subject):
+        self.name = name
+        self.subject = subject
+    def teach(self):
+        print(f"Учитель {self.teacher.name} преподает {self.teacher.subject}")
