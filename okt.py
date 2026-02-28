299dab5dfc7f1166121e962ff79b16377292c15d
Nikita <you@example.com>
2026-02-28 18:20:08 +0300

    Сделано

--- okt.py
+++ okt.py
@@ -5,3 +5,10 @@ class Car:
         self.age = age
     def introduce_car(self):
         print(f"Человек {self.name},  возраст {self.model},пол {self.age} ")
+class Exercise:
+    def __init__(self, name, calories_per_min ):
+        self.name = name
+        self.calories_per_min = calories_per_min
+    def get_info(self):
+        return (f"Название: {self.name}, Цена: {self.calories_per_min}")
+        
