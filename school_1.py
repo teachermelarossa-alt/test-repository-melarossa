5d839ae306fc1874a3620aa3d6c454e381539a27
Lera <you@example.com>
2026-02-28 18:32:10 +0300

    Добавила функцию

--- school_1.py
+++ school_1.py
@@ -1,6 +1,18 @@
+class Student:
+    def __init__(self,name,grade):
+        self.name=name
+        self.grade=grade
+    def get_status(self):
+        if self.grade > 8:
+            return "отлично"
+        else:
+            return "удовлетворительно"
+
+
 class Teacher:
     def __init__(self, name, subject):
         self.name = name
         self.subject = subject
     def teach(self):
         print(f"Учитель {self.teacher.name} преподает {self.teacher.subject}")
+
